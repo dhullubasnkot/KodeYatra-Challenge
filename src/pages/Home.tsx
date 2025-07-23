70b8d3ed9f136fcdc005679eea84f06c924622b9
dhullubasnkot <123baglung@gmail.com>
2025-07-23 14:31:48 +0545

    Updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -54,7 +54,7 @@ const Home = () => {
 
   return (
     <div className="min-h-screen bg-blue-100 p-6">
-      <h1 className="text-3xl font-bold text-center mb-6">Weather Dashboard</h1>
+      {/* <h1 className="text-3xl font-bold text-center mb-6">Weather</h1> */}
 
       <div className="flex justify-center mb-8">
         <input
